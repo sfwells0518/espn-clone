3779f66dc560cd9f0aef00536064af9ba4b9d2d8
Sam Wells <sfwells0518@gmail.com>
2023-03-30 16:16:33 -0500

    destroy model Sport

--- app/controllers/mlb_controller.rb
+++ app/controllers/mlb_controller.rb
@@ -1,2 +1,11 @@
 class MlbController < ApplicationController
+  def index
+    @ = Sport.all
+    render :index
+  end
+
+  def show
+    @ = Sport.find_by(id: params[:id])
+    render :show
+  end
 end
